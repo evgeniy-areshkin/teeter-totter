5f63b09aaf67f0625763c71d6cebde7406cd4533
Evgeniy Areshkin <evgeniy.areshkin@haymarket.in.ua>
2021-01-21 12:36:49 +0200

    fix

---
+++ dist/precache-manifest.f3cbae9b802fe57aa2ebcffb841ef107.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d50e206cb2036a2c79",
+    "url": "/css/app.0a158f3b.css"
+  },
+  {
+    "revision": "66ce1a42fd5cd2b9876f9384eee63b06",
+    "url": "/img/teeter.66ce1a42.svg"
+  },
+  {
+    "revision": "6b2aed577ef33c9682f851a9ef91ea9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d50e206cb2036a2c79",
+    "url": "/js/app.da03f8e6.js"
+  },
+  {
+    "revision": "946b760d41f81c45e07a",
+    "url": "/js/chunk-vendors.136a687a.js"
+  },
+  {
+    "revision": "55a29fdc890911bf78f22bb9947866aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
